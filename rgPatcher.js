1e1d26ef3aa9e620e76ed478d38645c068f08b85
DRMPLA <anton3112z@gmail.com>
2025-11-07 22:39:04 +0100

    Fix path of swf files

--- rgPatcher.js
+++ rgPatcher.js
@@ -1,67 +1,87 @@
-//therockstararchive Loader
+// therockstararchive Loader (universal version)
 
-//Load Ruffle as long as there's no 'noruffle' in the URL
-url = new URL(window.location.href);
-if (!window.location.href.includes("noruffle")) {
-	var script = document.createElement("script");
-    script.setAttribute("type", "text/javascript");
-    script.setAttribute("src", "https://unpkg.com/@ruffle-rs/ruffle");
-    document.head.appendChild(script);
-	
-	//Replace old video embeds with downloads
+// Detect current HTML base path dynamically
+(function() {
+    const pageURL = new URL(window.location.href);
+    // Strip filename (like index.html) and trailing slash to get clean directory
+    const basePath = pageURL.href.substring(0, pageURL.href.lastIndexOf('/') + 1);
+    console.log("Detected base path for SWFs:", basePath);
+
+    // Load Ruffle if not disabled
+    if (!window.location.href.includes("noruffle")) {
+        var script = document.createElement("script");
+        script.setAttribute("type", "text/javascript");
+        script.setAttribute("src", "https://unpkg.com/@ruffle-rs/ruffle");
+        document.head.appendChild(script);
+    }
+
     document.addEventListener('DOMContentLoaded', function() {
-    
-      // Grab all <embed> elements
-      var embeds = document.querySelectorAll('embed');
-    
-      embeds.forEach(function(embed, index) {
-        // Print the 'src' attribute for debugging
-        var src = embed.getAttribute('src');
-   
 
-        // If the src ends with .mov or .wmv (case-insensitive)
-        if (src && /\.(mov|wmv)$/i.test(src)) {
-        
+        // Replace old video embeds with download links (MOV/WMV)
+        var embeds = document.querySelectorAll('embed');
+        embeds.forEach(function(embed) {
+            var src = embed.getAttribute('src');
 
-          // Create a new anchor link to force a download
-          var link = document.createElement('a');
-          link.href = src;
-          link.download = '';
-		  link.style.color = '#000000';
-		  link.style.backgroundColor = '#ffffff';
-          // This sets the link text to something like "Download Max2-GamePlay-Clip11_ps2.mov"
-          link.textContent = 'Sadly, playback of MOV and WMV videos is not possible anymore in modern browsers, but files can be downloaded. Click here to download ' + (src.substring(src.lastIndexOf('/') + 1) || src);
+            if (src && /\.(mov|wmv)$/i.test(src)) {
+                var link = document.createElement('a');
+                link.href = src;
+                link.download = '';
+                link.style.color = '#000000';
+                link.style.backgroundColor = '#ffffff';
+                link.textContent = 'Sadly, playback of MOV and WMV videos is not possible anymore in modern browsers, but files can be downloaded. Click here to download ' + (src.substring(src.lastIndexOf('/') + 1) || src);
+                embed.parentNode.replaceChild(link, embed);
+            }
+        });
 
-          // Replace the <embed> with the <a> link
-          embed.parentNode.replaceChild(link, embed);
-        } 
-      });
+        // --- Universal SWF Fix ---
+        // Adjusts any <embed> or <object> with .swf that uses a relative path
+        function fixSWFPaths() {
+            // Fix <embed> SWFs
+            document.querySelectorAll('embed').forEach(function(embed) {
+                let src = embed.getAttribute('src');
+                if (src && /\.swf$/i.test(src) && !/^(https?:)?\/\//i.test(src)) {
+                    // Build correct URL relative to the HTML file’s directory
+                    const newSrc = new URL(src, basePath).href;
+                    embed.setAttribute('src', newSrc);
+                    console.log("Fixed SWF embed path:", newSrc);
+                }
+            });
+
+            // Fix <object> SWFs
+            document.querySelectorAll('object').forEach(function(obj) {
+                let data = obj.getAttribute('data');
+                if (data && /\.swf$/i.test(data) && !/^(https?:)?\/\//i.test(data)) {
+                    const newData = new URL(data, basePath).href;
+                    obj.setAttribute('data', newData);
+                    console.log("Fixed SWF object path:", newData);
+                }
+            });
+        }
+
+        fixSWFPaths();
     });
-	
-}
 
-window.RufflePlayer = window.RufflePlayer || {};
-window.RufflePlayer.config = {
-    // Options affecting the whole page
-    "publicPath": undefined,
-    "polyfills": true,
-
-    // Options affecting files only
-    "autoplay": "on",
-    "unmuteOverlay": "hidden",
-    "backgroundColor": null,
-    "wmode": "window",
-    "letterbox": "fullscreen",
-    "warnOnUnsupportedContent": false,
-    "contextMenu": true,
-    "showSwfDownload": true,
-    "upgradeToHttps": window.location.protocol === "https:",
-    "logLevel": "debug",
-    "base": null,
-    "menu": true,
-    "salign": "",
-    "scale": "showAll",
-    "forceScale": false,
-    "quality": "high",
-    "splashScreen": false,
-};
+    // Ruffle Config
+    window.RufflePlayer = window.RufflePlayer || {};
+    window.RufflePlayer.config = {
+        "publicPath": undefined,
+        "polyfills": true,
+        "autoplay": "on",
+        "unmuteOverlay": "hidden",
+        "backgroundColor": null,
+        "wmode": "window",
+        "letterbox": "fullscreen",
+        "warnOnUnsupportedContent": false,
+        "contextMenu": true,
+        "showSwfDownload": true,
+        "upgradeToHttps": window.location.protocol === "https:",
+        "logLevel": "debug",
+        "base": null,
+        "menu": true,
+        "salign": "",
+        "scale": "showAll",
+        "forceScale": false,
+        "quality": "high",
+        "splashScreen": false,
+    };
+})();
